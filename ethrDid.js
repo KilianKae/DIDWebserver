7f051cca3b81e221a9cbb6f5226762493216f3ac
Kilian Käslin <kilian.kaeslin@quartett-mobile.de>
2020-02-01 00:45:34 +0100

    - Updated EtherDid to store jwk for SIOP response. - Improved Website UI - Improved Server Logging

--- ethrDid.js
+++ ethrDid.js
@@ -1,17 +1,43 @@
 import EthrDID from 'ethr-did';
 import { Resolver } from 'did-resolver';
 import ethr from 'ethr-did-resolver';
-import didJWT, { verifyJWT, createJWT } from 'did-jwt';
+import didJWT, { verifyJWT } from 'did-jwt';
+import { toEthereumAddress } from 'did-jwt/lib/index';
+const EC = require('elliptic').ec;
+const secp256k1 = new EC('secp256k1');
 
 export default class EthrDid extends EthrDID {
   signer;
+  jwk;
 
-  constructor(args) {
-    super(args);
-    this.signer = didJWT.SimpleSigner(args.privateKey);
+  constructor({ provider, address, privateKey, jwk }) {
+    console.log('[EthrDID] New EtherDID');
+    super({ provider, address, privateKey });
+    this.jwk = jwk;
+    this.signer = didJWT.SimpleSigner(privateKey);
   }
 
-  //TODO try to remove / unistall resolvers
+  static createKeyPair() {
+    const kp = secp256k1.genKeyPair();
+    const x = kp
+      .getPublic()
+      .getX()
+      .toJSON();
+    const y = kp
+      .getPublic()
+      .getY()
+      .toJSON();
+    const publicKey = kp.getPublic('hex');
+    const privateKey = kp.getPrivate('hex');
+    const address = toEthereumAddress(publicKey);
+    const kid = `${address}#verikey-1`;
+    const crv = 'secp256k1';
+    const kty = 'EC';
+    const jwk = { crv, x, y, kty, kid };
+    return { address, privateKey, jwk };
+  }
+
+  //TODO try to remove, unistall resolvers
   async verifyJWT(jwt, audience = this.did) {
     const ethrResolver = ethr.getResolver();
     const resolver = new Resolver(ethrResolver);
@@ -20,24 +46,5 @@ export default class EthrDid extends EthrDID {
       audience
     });
     return verifiedJWT;
-  }
-
-  //TODO adjust to client implementation
-  signJWT(content, aud) {
-    const alg = 'ES256K-R';
-    const name = 'DIDWebserver';
-    const exp = Date.now() / 1000 + 3600;
-    let payload = {
-      aud,
-      exp,
-      name
-    };
-    payload = { ...payload, ...content };
-    const options = {
-      alg,
-      issuer: this.did,
-      signer: this.signer
-    };
-    return createJWT(payload, options);
   }
 }
--- server.js
+++ server.js
@@ -1,33 +1,66 @@
 'use strict';
 import express from 'express';
 import path from 'path';
-import querystring from 'querystring';
+import url from 'url';
 import DidManager from './didManager.js';
 import crypto from 'crypto';
 
 const app = express();
-
-var getResolver = require('ethr-did');
-var EthrResolver = require('ethr-did-resolver');
+const port = '8080';
 
 const didManager = new DidManager();
-const port = '8080';
-//TODO create supfiles with logic
+
+const logger = function(req, res, next) {
+  console.log('[server] Request URL:', req.originalUrl);
+  next();
+};
+
+app.use(logger);
+
+app.listen(port, () =>
+  console.log('Server started at http://localhost:' + port)
+);
+
+//TODO create subfiles with logic
 
 // viewed at http://localhost:port
 app.get('/', function(req, res) {
-  console.log('[server] Received get / request');
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/credential', function(req, res) {
+  res.sendFile(path.join(__dirname + '/credential.html'));
+});
+
 app.get('/newDid', function(req, res) {
-  console.log('[server] Received get /newDid request');
   didManager.newEthrDid();
   res.redirect('/');
 });
 
+app.get('/credentialRequest', function(req, res) {
+  //TODO set claim && https://www.w3.org/TR/vc-data-model/
+  const claim = {
+    credentialSubject: {
+      id: 'please set',
+      group: 'admin'
+    }
+  };
+  createCredential(claim)
+    .then(credential => {
+      console.log('[server] credential', credential);
+      const url = createCredentialUrl(credential);
+      console.log('[server] Redirecting to', url);
+      res.redirect(url);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: 'Internal Server Error',
+        message: error.toString()
+      });
+    });
+});
+
 app.get('/siopRequest', function(req, res) {
-  console.log('[server] Received get /siopRequest request');
   createLoginJWT()
     .then(jwt => {
       const url = createLoginUrl(jwt);
@@ -39,18 +72,17 @@ app.get('/siopRequest', function(req, res) {
 
 //TODO should be post request
 app.get('/siopResponse', function(req, res) {
-  console.log('[server] Received get /siopResponse request');
   //TODO check if id_token is available
   const id_token = req.query.id_token;
   console.log('[server] req id_token', id_token);
   //TODO add database check & token generation
   validateSiopResponse(id_token)
     .then(obj => {
-      res.status(200).json({ login: true });
+      //res.status(200).json({ login: true });
+      res.redirect('/credential');
     })
     .catch(error => {
       //TODO correct error message
-      console.error('[server] error', error);
       res.status(400).json({
         error: 'Bad Request',
         message: error.toString()
@@ -58,28 +90,48 @@ app.get('/siopResponse', function(req, res) {
     });
 });
 
-app.listen(port, () =>
-  console.log('Server started at http://localhost:' + port)
-);
-
-function createLoginJWT() {
+async function createLoginJWT() {
   const nonce = crypto.randomBytes(24).toString('Base64');
-  return didManager.ethrDid.signJWT({ nonce });
+  const payload = {
+    response_type: 'id_token',
+    client_id: 'http://localhost:8080',
+    scope: 'openid did_authn',
+    nonce,
+    response_mode: 'query'
+    // registration: {
+    //   jwks_uri:
+    //     'https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks',
+    //   id_token_signed_response_alg: ['ES256K', 'EdDSA', 'RS256']
+    // }
+  };
+  return await didManager.ethrDid.signJWT({ payload });
 }
 
 function createLoginUrl(jwt) {
-  const base = 'didapp://login?';
-  const query = {
-    response_type: 'id_token',
-    client_id: 'http://localhost:8080',
-    scope: 'myid%20did_authn',
-    request: jwt
-  };
-  const url = base + querystring.stringify(query);
-  return url;
+  return url.format({
+    pathname: 'didapp://login',
+    query: {
+      response_type: 'id_token',
+      client_id: 'http://localhost:8080',
+      scope: 'myid%20did_authn',
+      request: jwt
+    }
+  });
 }
 
-function validateSiopResponse(jwt) {
+async function validateSiopResponse(jwt) {
   //Use Object Values?
-  return didManager.ethrDid.verifyJWT(jwt);
+  return await didManager.ethrDid.verifyJWT(jwt);
+}
+
+async function createCredential(claim) {
+  //TODO get DID of requester
+  return await didManager.ethrDid.signJWT({ claim });
+}
+
+function createCredentialUrl(credential) {
+  return url.format({
+    pathname: 'didapp://credential',
+    query: { credential }
+  });
 }
