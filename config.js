442f4628be49e2c38477bf7c898df740944898ba
Kilian Käslin <kilian.kaeslin@quartett-mobile.de>
2020-03-31 18:10:49 +0200

    - Moved to localhost

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //Configuration variables
 const port = process.env.PORT || '8080';
-const ip = process.env.IP || '192.168.0.2';
+const ip = process.env.IP || 'localhost'; //'192.168.0.2';
 
 module.exports = {
   port,
